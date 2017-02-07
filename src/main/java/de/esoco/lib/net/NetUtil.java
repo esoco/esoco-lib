8587e38aadeaeb8ec256a84114bd8b82d264f601
Elmar Sonnenschein <eso@esoco.de>
2017-02-07 10:54:28 +0100

    BF: invalid import corrected

--- src/main/java/de/esoco/lib/net/NetUtil.java
+++ src/main/java/de/esoco/lib/net/NetUtil.java
@@ -18,6 +18,7 @@ package de.esoco.lib.net;
 
 import de.esoco.lib.io.StreamUtil;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
@@ -48,8 +49,6 @@ import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
-
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 
 
 /********************************************************************
@@ -372,15 +371,15 @@ public class NetUtil
 		rOutputStream.write(aRequestBytes);
 		rOutputStream.flush();
 
-		ByteOutputStream aOutput = new ByteOutputStream();
-		byte[]			 aReply  = null;
+		ByteArrayOutputStream aOutput = new ByteArrayOutputStream();
+		byte[]				  aReply  = null;
 
 		if (StreamUtil.readUntil(rProxySocket.getInputStream(),
 								 aOutput,
 								 "\r\n\r\n".getBytes(TUNNELING_CHARSET),
 								 512))
 		{
-			aReply = aOutput.getBytes();
+			aReply = aOutput.toByteArray();
 		}
 
 		if (aReply != null)
