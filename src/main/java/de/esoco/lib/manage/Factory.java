18e42d6fc61bd86612d2fd658001b82f4dca8c4b
Elmar Sonnenschein <eso@esoco.de>
2018-07-16 09:49:24 +0200

    Annotation added

--- src/main/java/de/esoco/lib/manage/Factory.java
+++ src/main/java/de/esoco/lib/manage/Factory.java
@@ -1,6 +1,6 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // This file is a part of the 'esoco-lib' project.
-// Copyright 2015 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+// Copyright 2018 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ package de.esoco.lib.manage;
  *
  * @author eso
  */
+@FunctionalInterface
 public interface Factory<T, D extends ElementDefinition<T>, E extends Exception>
 {
 	//~ Methods ----------------------------------------------------------------
